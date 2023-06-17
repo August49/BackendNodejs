3174e9ab8267e3ad64ea9215142f506a6f838a2e
Joseph Ngongo <augustiniusngongo@gmail.com>
2023-06-18 04:45:10 +0530

    building RestFULApi

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
 const express = require("express");
-
+const movieDB = require("./moviesApiDummyDB");
 const app = express(); //web application
 
 app.use(express.json());
 
 //route
-app.get("/", (req, res) => {
+//Handling get request
+app.get("/api", (req, res) => {
   res.send("Hello world from server");
   res.end();
+});
+
+app.get("/api/movies", (req, res) => {
+  res.send(movieDB);
+});
+
+app.get("/api/movies/:title", (req, res) => {
+  const movie = movieDB.find((title) => title.title === req.params.title);
+  if (!movie)
+    res.status(404).send("Movie with the given title cannot be found");
+  res.send(movie);
+  //query
+});
+
+//Handling post request
+
+app.post("/api/movies", (req, res) => {
+  const newMovie = {
+    id: movieDB.length + 1,
+    title: req.body.title,
+    rating: req.body.rating,
+    genre: req.body.genre,
+  };
+
+  movieDB.push(newMovie);
+  res.send(newMovie);
 });
 
 //listening Port
