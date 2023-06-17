bf4712eccc9b12daad069acbce20a6db58aafdd1
Joseph Ngongo <augustiniusngongo@gmail.com>
2023-06-18 04:46:15 +0530

    dummy movieApiDB

---
+++ moviesApiDummyDB.js
@@ -0,0 +1,14 @@
+const movieDB = [
+  { id: 1, title: "Honorable", rating: 1.1, genre: "Action" },
+  { id: 2, title: "Ms", rating: 2.3, genre: "Adventure" },
+  { id: 3, title: "Ms", rating: 2.1, genre: "Action" },
+  { id: 4, title: "Honorable", rating: 2.1, genre: "Adventure" },
+  { id: 5, title: "Ms", rating: 4.6, genre: "Sci-Fi" },
+  { id: 6, title: "Dr", rating: 2.5, genre: "Sci-Fi" },
+  { id: 7, title: "Dr", rating: 2.2, genre: "Adventure" },
+  { id: 8, title: "Mrs", rating: 3.2, genre: "Action" },
+  { id: 9, title: "Rev", rating: 1.7, genre: "Action" },
+  { id: 10, title: "Mrs", rating: 3.2, genre: "Sci-Fi" },
+];
+
+module.exports = movieDB;
